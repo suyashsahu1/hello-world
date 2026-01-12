d4ffd55ca624ba78e01593d45288954741670e6d
Suyash Sahu <suyash.sahu@geminisolutions.com>
2026-01-12 23:59:01 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is a sample React application.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.geminisolutions.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello From Gemini Solutions!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
